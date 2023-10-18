adc9c1494ade3b8802213b8c165e3c4da8571cc3
Viktor Olenberg <viktor.olenberg@outlook.com>
2023-10-18 21:55:25 +0200

    unfertiges Login
    Authentication noch nicht mit der Datenbank Verknüpft

--- SSPT-ACDT-ISMS-Project/Program.cs
+++ SSPT-ACDT-ISMS-Project/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SSPT_ACDT_ISMS_Project.mod;
+using System.Net;
 
 namespace SSPT_ACDT_ISMS_Project
 
@@ -6,9 +7,40 @@ namespace SSPT_ACDT_ISMS_Project
     internal class Program
     {
 
-
-        // Menü des Programms - menü Punkte hier zu bearbeiten
         static void Main(string[] args)
+        {
+            while (true)
+            {
+                Login();
+            }
+            
+        }
+
+
+        static void Login()
+        {
+            // Sorgt dafür, dass man trotz nicht Vertrauenswürdiger SSL Verbindung connecten kann
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            string connectionString = "Data Source=ZYBERTRACK-PC\\SQLEXPRESS;Database=ISMS-Repository";
+            DatabaseLogin login = new DatabaseLogin(connectionString);
+
+            Console.Write("Benutzername: ");
+            string username = Console.ReadLine();
+            Console.Write("Passwort: ");
+            string password = Console.ReadLine();
+
+            bool loggedIn = login.Authenticate(username, password);
+
+            if (loggedIn == true)
+            {
+                MenuDisplay(); // Hier können Sie den angemeldeten Benutzer weiterleiten oder andere Aktionen ausführen.
+            }
+
+        }
+
+
+        static void MenuDisplay()   // Menü des Programms - menü Punkte hier zu bearbeiten
         {
             List<string> menuOptions = new List<string>
         {
---
+++ SSPT-ACDT-ISMS-Project/mod/login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using Microsoft.Data.SqlClient;
+
+namespace SSPT_ACDT_ISMS_Project.mod
+{
+
+
+    public class DatabaseLogin
+    {
+        private string connectionString; // Verbindungszeichenfolge zur Datenbank
+
+        public DatabaseLogin(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+
+                // SQL-Abfrage, um Benutzerdaten abzurufen
+                string query = "SELECT COUNT(*) FROM Benutzer WHERE Benutzername = @Username AND Passwort = @Password";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.Add(new SqlParameter("@Username", SqlDbType.VarChar)).Value = username;
+                    command.Parameters.Add(new SqlParameter("@Password", SqlDbType.VarChar)).Value = password;
+
+                    int userCount = (int)command.ExecuteScalar();
+
+                    if (userCount == 1)
+                    {
+                        Console.WriteLine("Anmeldung erfolgreich.");
+                        connection.Close();
+                        return true;
+                    }
+                }
+                connection.Close();
+            } // Die Verbindung wird automatisch geschlossen, wenn der using-Block verlassen wird.
+
+            Console.WriteLine("Anmeldung fehlgeschlagen. Bitte überprüfen Sie Ihren Benutzernamen und Ihr Passwort.");
+            return false;
+        }
+    }
+}
